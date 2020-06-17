d387a5cd28569ce667b5705675bbe62ade7b85d3
nojaewoong <55523998+nojaewoong@users.noreply.github.com>
2020-06-18 08:31:38 +0900

    Add files via upload

---
+++ card.c
@@ -0,0 +1,79 @@
+struct card
+{
+	char order;
+	char number;
+}
+
+struct trump
+{
+	char order;
+	char shape[3];
+	char number;
+}
+
+trump card[52];
+
+int i,j;
+char shape[4][3]={"◆","♠","♣","♥"};
+for(i=0; i<4; i++)
+{
+	for(j=i*13; j<i*13+13; j++)
+	{
+		m_card[j].order=i;
+		strcpy(m_card[j].shape,shape[i]);
+		m_card[j].number=j%13+1;
+		switch(m_card[j].number)
+		{
+			m_card[j].number에 따라 A,J,Q,K를 저장 
+		}
+	}
+}
+
+void make_card(trump m_card[])
+{
+	int i,j;
+	char shape[4][3]={"◆","♠","♣","♥"};
+	for(i=0; i<4; i++)
+{
+	for(j=i*13; j<i*13+13; j++)
+	{
+		m_card[j].order=i;
+		strcpy(m_card[j].shape,shape[i]);
+		m_card[j].number=j%13+1;
+		switch(m_card[j].number)
+		{
+			case 1:
+			    m_card[j].number='A';
+			    break;
+			case 11:
+			    m_card[j].number='J';
+			    break;
+			case 12:
+			    m_card[j].number='Q';
+			    break;
+			case 13:
+			    m_card[j].number='K';
+			    break;
+		}
+	}
+}
+}
+
+void display_card(trump m_card[])
+{
+	int i, count=0;
+	for(i=0; i<52; i++)
+	{
+		printf("%s",m_card[i].shape);
+		if (10<m_card[i].shape)
+		    printf("%-2c",m_card[i].shape);
+		else
+		    printf("%-2d",m_card[i].shape);
+		count++;
+		if (i%13+1==13)
+		{
+			printf("\n");
+			count=0;
+		}
+	}
+}
